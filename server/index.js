e1806b64c1b4ce60b8a081388ded3d5e6cb8af62
David <david.samacoits@gmail.com>
2019-07-06 11:30:47 -0400

    Added socket io

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+const port = 3000;
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
 });
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(port, () => {
+  console.log(`📣 Listening on port ${port}`);
 });
