8c07af2d280a57dd138184f6ff529f8a0127852c
David <david.samacoits@gmail.com>
2019-07-06 11:41:28 -0400

    Linked client with server

--- client/app.jsx
+++ client/app.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
+import socketIOClient from "socket.io-client";
 
-class HelloMessage extends React.Component {
-  render() {
-    return <div> Hello {this.props.name} </div>;
-  }
-}
+import { SERVER_ENDPOINT } from "./config";
 
-const mountNode = document.getElementById("root");
-ReactDOM.render(<HelloMessage name="Jane" />, mountNode);
+const App = props => {
+  useEffect(() => {
+    // Initializing socket
+    const socket = socketIOClient(SERVER_ENDPOINT);
+    // socket.on("change color", col => {
+    //   document.body.style.backgroundColor = col;
+    // });
+  });
+
+  return <div> Hello </div>;
+};
+
+const root = document.getElementById("root");
+ReactDOM.render(<App />, root);
